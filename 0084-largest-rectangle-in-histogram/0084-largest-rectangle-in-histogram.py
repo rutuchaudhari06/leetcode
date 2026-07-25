75e86f4989a7f0471294de41c6d5a32a660b2e50
Rutu <161607809+rutuchaudhari06@users.noreply.github.com>
2026-07-25 12:21:53 +0530

    Time: 103 ms (96.11%), Space: 30.5 MB (88.43%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,34 +1,22 @@
 class Solution:
-    def findnse(self,nums):
-        n=len(nums)
-        nse=[n]*n
-        st=[]
-        for i in range(n-1,-1,-1):
-            while st and nums[st[-1]]>=nums[i]:
-                st.pop()
-            nse[i]=n if not st else st[-1]
-            st.append(i)
-        return nse
-
-    def findpse(self,nums):
-        n=len(nums)
-        pse=[-1]*n
-        st=[]
-        for i in range(n):
-            while st and nums[i]<nums[st[-1]]:
-                st.pop()
-            pse[i]= -1 if not st else st[-1]
-            st.append(i)
-        return pse
-
     def largestRectangleArea(self, nums: List[int]) -> int:
-        nse=self.findnse(nums)
-        pse=self.findpse(nums)
         maxarea=0
         n=len(nums)
 
+        st=[]
+
         for i in range(n):
-            maxarea=max(maxarea,nums[i] * (nse[i]-pse[i]-1))
+            while st and nums[st[-1]]>=nums[i]:
+                el=st.pop()
+                nse=i 
+                pse= -1 if not st else st[-1]
+                maxarea=max(maxarea,nums[el] * (nse-pse-1))
+            st.append(i)    
+        while st:
+            el = st.pop()
+            nse = n
+            pse = -1 if not st else st[-1]
+            maxarea = max(maxarea, nums[el] * (nse - pse - 1))      
         return maxarea
 
 
